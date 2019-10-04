0a39cf488bc291d3d1b9ecb3adb7c5d04feb2097
ahadsenja <ahadsenja@MacBook-Pro-macbook.local>
2019-10-04 22:32:58 +0800

    chore: updating app file

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import { Row, Col } from 'react-bootstrap'
 import ImageCanvas from './components/ImageCanvas/ImageCanvas'
 import HeaderMenu from './components/HeaderMenu/HeaderMenu'
 import SideMenu from './components/SideMenu/SideMenu'
+import LabelingForm from './components/LabelingForm/LabelingForm'
 
 class App extends React.Component {
   render(){
     return (
       <div className='App'>
-        <Row>
-          <Col> <HeaderMenu /> </Col>
-        </Row>
+        <Row><Col> <HeaderMenu /> </Col></Row>
+
         <Row>
           <Col xs='2'> <SideMenu /> </Col>
-          <Col xs='8'> <ImageCanvas /> </Col>
-          <Col xs='2'>  </Col>
+          <Col xs='8'> 
+            <ImageCanvas /> 
+          </Col>
+          <Col style={{ paddingRight: 20 }} xs='2'> 
+            <LabelingForm /> 
+          </Col>
         </Row>
       </div>
     )
