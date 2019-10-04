6c6fc6b530436142b771586d6c8ad40497a366bc
ahadsenja <hamdanilm96@gmail.com>
2019-10-04 22:41:59 +0800

    chore: updating labeling form directory

--- src/components/LabelingForm/LabelingForm.js
+++ src/components/LabelingForm/LabelingForm.js
@@ -1,44 +1,28 @@
 import React from 'react'
-import { Form, Modal, FormLabel, FormControl, Button } from 'react-bootstrap'
+import { Form, Button, Card } from 'react-bootstrap'
 
 class LabelingForm extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            show: false   
-        }
-    }
-    show = () => {
-        this.setState({ show: !this.state.show })
-    }
-
     render() {
         return (
             <div>
-                <Button onClick={this.show}>Labeling</Button>
+                <Card style={{ padding: 10 }}>
+                    <Form.Text><strong>Labeling Form</strong></Form.Text>
+                    <hr />
+                    <Form>
+                        <Form.Group>
+                            <Form.Label style={{ fontSize: 12 }}>Classification</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
 
-                <Modal show={this.state.show} onHide={this.show}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Labeling Image</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form>
-                            <Form.Group>
-                                <FormLabel>Classification</FormLabel>
-                                <FormControl />
-                            </Form.Group>
-
-                            <Form.Group>
-                                <FormLabel>Description</FormLabel>
-                                <FormControl />
-                            </Form.Group>
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant='secondary' onClick={this.show}>Cancel</Button>
-                        <Button variant='info'>Ok</Button>
-                    </Modal.Footer>
-                </Modal>
+                        <Form.Group>
+                            <Form.Label style={{ fontSize: 12 }}>Description</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                        <Button size='sm' variant='secondary' onClick={this.toggleShowForm}>Cancel</Button>
+                        <span style={{ paddingLeft: 5 }}></span>
+                        <Button size='sm' variant='info'>Ok</Button>
+                    </Form>
+                </Card>
             </div>
         )
     }
